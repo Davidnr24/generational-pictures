dd29d65e0af99d9f0dc56867edaf494a4c7ec6fb
Dhakernator24 <davidn.snow@hotmail.es>
2023-10-16 19:55:54 -0400

    img hover fix

--- src/components/VideoWorkCard.jsx
+++ src/components/VideoWorkCard.jsx
@@ -11,15 +11,15 @@ import storytelling_image from '../assets/storytelling.png';
 export default function VideoWorkCard({ name, subtitle, imageC, href, avg_color }) {
 
 	return (
-		<Link to={href} className='group/work relative h-[40vh] w-auto hover:shadow-xl transition duration-500 '>
+		<Link to={href} className='group/work relative h-[40vh] w-auto hover:shadow-xl transition duration-500 overflow-hidden '>
 			<img
-				src={imageC} className="h-full w-full object-cover"
+				src={imageC} className="h-full w-full object-cover group-hover/work:scale-110 transition duration-300"
 			/>
 			<div className={`absolute top-0 left-0 items-start z-2 w-full h-full ${avg_color} flex flex-col justify-end
 			group-hover/work:backdrop-contrast-200 group-hover/work:bg-transparent transition duration-500`}>
 				<div className='flex flex-col border-logo-white ml-10 mb-3 p-3 border-[1px] '>
-					<div className='text-logo-white text-xl'>{name}</div>
-					<div className='text-logo-white text-lg'>{subtitle}</div>
+					<div className='text-logo-white text-[35px]'>{name}</div>
+					<div className='text-logo-white text-[21px]'>{subtitle}</div>
 				</div>
 			</div>
 		</Link>
