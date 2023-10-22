20308eee6d0b63f1e096979261366aca4053bfe6
Dhakernator24 <davidn.snow@hotmail.es>
2023-10-22 11:53:52 -0400

    fix

--- src/components/VideoComp.jsx
+++ src/components/VideoComp.jsx
@@ -8,7 +8,7 @@ export default function VideoComp({ title, url, img_src, preview_src }) {
   const [isPlaying, setIsPlaying] = useState(false)
 
   return (
-    <div className="m-4 flex flex-col justify-center items-center">
+    <div className="m-4 flex flex-col justify-center gap-3 items-center">
       <div className="font-poppins text-[20px] text-black">{title}</div>
       <div 
       onMouseEnter={() => setIsPlaying(true)} 
