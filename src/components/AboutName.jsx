167ac37f57a61aa8ae763912f87f020386514668
Dhakernator24 <davidn.snow@hotmail.es>
2023-12-26 19:11:10 -0500

    Fix mobile about

--- src/components/AboutName.jsx
+++ src/components/AboutName.jsx
@@ -4,7 +4,7 @@
 export default function AboutName( { name, ps, pic, bgPos="bg-center", bgSize="bg-cover" }) {
 
   return (
-    <div className='flex md:flex-row even:flex-row-reverse flex-col gap-5 md:gap-0 w-full mt-16'>
+    <div className='flex md:flex-row lg:even:flex-row-reverse flex-col gap-5 md:gap-0 w-full mt-16'>
       <div className='flex justify-start lg:justify-center items-center md:w-[40%] w-full'>
       <div className={`h-[300px] bg-logo w-[300px] rounded-full ${bgPos} ${bgSize}`}
             style={{ backgroundImage: `url(${pic})` }} />
