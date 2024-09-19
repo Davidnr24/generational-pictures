76b2eed3426de766721923a338f41249eee7775c
David <davidn.snow@hotmail.es>
2024-09-19 18:19:36 -0400

    Change Request 9/19

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,14 +9,9 @@ function About() {
         {
             name: 'Felícita',
             ps: [
-                'Felícita Kostianis is co-founder, majority owner, driven entrepreneur, and a proud Puerto Rican and Greek woman. Motivated by her empathy, curiosity, and passion for creating positive change in the world, \
-                she joined forces with her father to build a video production company aimed at addressing diversity challenges in the industry and helping clients successfully reach diverse demographics. \
-                She is dedicated to creating a space within the industry that not only produces high-quality work but also uplifts underrepresented voices, \
-                aspiring to carve a path that champions both excellence and diversity in filmmaking.',
-
-                'Beyond her commitment to learning and developing the business, Felícita loves to travel, \
-                with her best trip as of late being a road trip spanning the north of Spain. Her journey reflects a dedication \
-                to reshaping narratives and fostering inclusivity and positivity in a world that needs it.'
+                'Felícita Kostianis is the majority owner, a passionate Producer, and a proud Puerto Rican and Greek woman. Motivated by her empathy, curiosity, and passion for creating positive change in the world, she joined forces with her father to build a video production company aimed at addressing diversity challenges in the industry and helping clients successfully reach diverse demographics. She is dedicated to creating a space within the industry that not only produces high-quality work but also uplifts voices, aspiring to carve a path that champions both excellence and diversity in filmmaking.'
+                ,
+                'Beyond her commitment to learning and storytelling, Felícita loves to travel, with her best trip as of late being a road trip spanning the north of Spain.'
             ],
             pic: felicita_image,
             bgPos: "[background-position:20%_40%]",
@@ -25,12 +20,9 @@ function About() {
         {
             name: 'Chris',
             ps: [
-                'With over 30 years of experience, Chris Kostianis, co-founder of Generational Pictures, brings a keen eye for detail and a passion for captivating visuals. \
-                With a diverse background spanning narratives, documentaries, commercials, television shows, and promotional videos, Chris offers a fresh and polished perspective to projects.',
-
-                'He has collaborated with numerous notable clients throughout his career, including ESPN, Showtime, ABC, Nickelodeon, CBS, VH1, the New York Mets,the NHL, MSG Network, and BET, among others. \
-                The results-driven approach that he brings to each project is client-oriented, as his objective is to help you achieve your goals. Whether delivering a refined look, eye-catching visuals, \
-                or evoking energy, Chris and his dynamic lighting and camera movements translate concepts into powerful visual narratives.'
+                'With over 25 years of experience and more than 10 Emmys, Chris Kostianis brings a keen eye for detail and a passion for captivating visuals. With a diverse background spanning narratives, documentaries, commercials, television shows, and promotional videos, Chris offers a fresh and polished perspective to projects.'
+                ,
+                'His results-driven approach to each project is client-oriented, always focused on helping you achieve your goals. Whether delivering a refined look, eye-catching visuals, or evoking energy, Chris uses his dynamic lighting and camera movements to translate concepts into powerful visual narratives.'
             ],
             pic: chris_image,
             bgPos: "[background-position:50%_50%]",
@@ -43,11 +35,13 @@ function About() {
             <div className='flex flex-col mt-[50px] justify-start items-start gap-3 lg:w-1/2'>
                 <h1 className='font-zenKaku text-white text-4xl w-full font-semibold [text-align:center] md:[text-align:left]'>Our Story</h1>
                 <p className='font-zenKaku text-white h-fit w-full [text-align:center] md:[text-align:left]'>
-                    Generational Pictures is a woman-owned video production company backed by the expertise of co-founders 
-                    Felícita Kostianis and Emmy Award Winning Director of Photography, Chris Kostianis. 
-                    Founded by the father-daughter duo, we are bringing their above-and-beyond mindset to the level of a full-service production and post-production company. 
-                    By constantly questioning, “What’s impossible that we can achieve through our work?” 
-                    Generational Pictures is a company dedicated to making our clients’ visions into extraordinary visual realities.
+                Generational Pictures is a woman-owned video production company led by Felícita Kostianis and Chris Kostianis. Founded by this father-daughter duo, we provide exceptional full-service production and post-production solutions, driven by a fresh perspective and commitment to impressive results.
+                </p>
+                <p className='font-zenKaku text-white h-fit w-full [text-align:center] md:[text-align:left]'>
+                At Generational Pictures, we understand that people engage with videos in unique ways. Drawing on our co-founders’ multicultural backgrounds and diverse generational insights, our team’s approach ensures that every project connects authentically with audiences across various generations and cultures.
+                </p>
+                <p className='font-zenKaku text-white h-fit w-full [text-align:center] md:[text-align:left]'>
+                By embracing challenges with innovative thinking and drawing on our team’s seasoned expertise, Generational Pictures transforms our clients’ visions into stunning content.
                 </p>
             </div>
             <div className='mt-4 h-1 w-[200px] lg:w-1/2 bg-logo [margin-left:auto] [margin-right:auto] md:[margin-left:0px] md:[margin-right:0px]' />
